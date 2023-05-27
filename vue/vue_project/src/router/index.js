c5e17ff7158d0e37e04d28c3052eaa5e9c3c12d5
Iwanami Sakitaro <a5819014@aoyama.jp>
2023-05-27 19:53:48 +0900

    create oldVueToDo base

--- vue/vue_project/src/router/index.js
+++ vue/vue_project/src/router/index.js
@@ -3,7 +3,9 @@ import VueRouter from 'vue-router'
 import TopPage from '@/views/TopPage'
 import SamplePage from '@/views/SamplePage'
 import PracticePage from '@/views/PracticePage'
-import OldVueToDo from '@/views/OldVueToDo'
+import OldVueHomePage from '@/views/OldVueHomePage'
+import OldVueIndexPage from '@/views/OldVueIndexPage'
+import OldVueNewPage from '@/views/OldVueNewPage'
 
 Vue.use(VueRouter)
 
@@ -25,8 +27,18 @@ const routes = [
   },
   {
     path: '/oldtodo',
-    name: 'OldVueToDo',
-    component: OldVueToDo
+    name: 'OldVueHome',
+    component: OldVueHomePage
+  },
+  {
+    path: '/oldtodo/index',
+    name: 'OldVueIndex',
+    component: OldVueIndexPage
+  },
+  {
+    path: '/oldtodo/index/new',
+    name: 'OldVueNew',
+    component: OldVueNewPage
   }
 ]
 
---
+++ vue/vue_project/src/views/OldVueNewPage.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="New">
+      <navbar></navbar> 
+      <h1 class="title">New</h1>
+      <div>
+        <input v-model="newTodo" type="text" placeholder="Enter new task">
+        <button @click="addTodo">Add</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Navbar from '@/components/Navbar.vue';
+  
+  export default {
+    name: 'OldVueNew',
+    components: {
+      Navbar,
+    },
+    data() {
+      return {
+        newTodo: '',
+      };
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo.trim() !== '') {
+                this.$store.dispatch('addTodo', this.newTodo).then(() => {
+                this.newTodo = '';
+                this.$store.dispatch('loadTodos').then(() => {
+                    this.$router.push('/oldtodo/index');
+                });
+                });
+            }
+        },
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .title {
+    margin-left: 40px;
+  }
+  </style>
+  
